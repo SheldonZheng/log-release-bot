aeccd9faad0ee4a08210b73d1f19bdbe47ad3f51
Baiye <zhenghangtxdyr@gmail.com>
2017-07-08 13:58:11 +0800

    token存入文件;

--- bot.py
+++ bot.py
@@ -2,11 +2,18 @@ import requests
 
 token = ''
 
+def getToken() :
+    with open('token','rt',encoding='utf-8') as f:
+        global token
+        token = f.read()
+        #print(token)
+
 def getUpdates() :
     url = 'https://api.telegram.org/bot%s/getUpdates' % token
     r = requests.get(url)
     return r.text
 
 if __name__ == '__main__' :
+    getToken()
     updates = getUpdates()
     print(updates)
