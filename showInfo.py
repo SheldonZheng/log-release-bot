29472713565efadd0e3067b318f4ef67b79b4aaa
Baiye <zhenghangtxdyr@gmail.com>
2017-07-10 21:21:54 +0800

    支持新建报告查询等多个命令;

--- showInfo.py
+++ showInfo.py
@@ -2,6 +2,9 @@ from flask import Flask
 from flask import render_template, request
 import telegram
 import logging
+from sqlalchemy import Column, String, create_engine,Integer,Boolean,DateTime
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 from OpenSSL import SSL
 
 logging.basicConfig(level=logging.DEBUG,
@@ -15,6 +18,24 @@ bot = telegram.Bot(token='')
 
 bot_name = '@'
 
+
+Base = declarative_base()
+
+class LogRelease(Base) :
+    __tablename__ = 'log_release'
+
+    id = Column(Integer,primary_key=True)
+    project_name = Column(String(100),index=True)
+    tag_name = Column(String(100),index=True)
+    result = Column(String(100))
+    operator = Column(String(100))
+    status = Column(Boolean)
+    created_at = Column(DateTime)
+    updated_at = Column(DateTime)
+
+engine = create_engine('mysql+mysqlconnector://')
+
+DBSession = sessionmaker(bind=engine)
 
 @app.route('/<token>',methods=['POST'])
 def showInfo(token) :
@@ -33,6 +54,12 @@ def handle(message) :
     print(type(text))
     if '/echo' in text :
         echo(message)
+    elif '/new' in text :
+        newLogRelease(message)
+    elif '/report' in text :
+        reportResult(message)
+    elif '/queryByOperator' in text :
+        queryByOperator(message)
 
 def echo(meesage) :
     cmd,text = parse_cmd_text(meesage.text)
@@ -41,6 +68,55 @@ def echo(meesage) :
     else :
         chat_id = meesage.chat_id
         bot.sendMessage(chat_id=chat_id,text=text)
+
+
+def newLogRelease(meesage) :
+    cmd,text = parse_cmd_text(meesage.text)
+    if None == text or len(text) == 0 :
+        pass
+    else :
+        textSplited = text.split('/')
+
+        session = DBSession()
+
+        new_log = LogRelease(project_name = textSplited[0],tag_name=textSplited[1],operator=textSplited[2],result='UNCOMMIT',status = True)
+        session.add(new_log)
+        session.commit()
+        session.close()
+        chat_id = meesage.chat_id
+        bot.sendMessage(chat_id=chat_id,text='创建新的上线记录成功')
+
+def reportResult(meesage) :
+    cmd,text = parse_cmd_text(meesage.text)
+    if None == text or len(text) == 0 :
+        pass
+    else :
+        textSplited = text.split('/')
+        session = DBSession()
+        logrelease = session.query(LogRelease).filter(LogRelease.project_name==textSplited[0],LogRelease.tag_name==textSplited[1]).first();
+        if logrelease == None :
+            chat_id = meesage.chat_id
+            bot.sendMessage(chat_id=chat_id,text='未查询到对应的上线记录')
+        logrelease.result = textSplited[2]
+        session.add(logrelease)
+        session.commit()
+        session.close()
+        chat_id = meesage.chat_id
+        bot.sendMessage(chat_id=chat_id, text='汇报上线记录状态成功')
+
+def queryByOperator(meesage) :
+    cmd,text = parse_cmd_text(meesage.text)
+    if None == text or len(text) == 0:
+        pass
+    else:
+        session = DBSession()
+        logreleases = session.query(LogRelease).filter(LogRelease.operator==text).all()
+        result = ''
+        for logrelease in logreleases :
+            result = result + logrelease.project_name + '/' + logrelease.tag_name + '/' + logrelease.result + '\n'
+        chat_id = meesage.chat_id
+        bot.sendMessage(chat_id=chat_id, text=result)
+
 
 def parse_cmd_text(text):
     # Telegram understands UTF-8, so encode text for unicode compatibility
